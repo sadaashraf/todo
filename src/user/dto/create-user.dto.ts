8a6d1bf6a56809035a81178512e61a580e105bb6
Muhammad Ashraf <nayeemashraf92@gmail.com>
2026-01-05 16:39:35 +0500

    dto add

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { IsEmail, IsString } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  firstName: string;
+  @IsString()
+  lastName: string;
+  @IsEmail()
+  email: string;
+  @IsString()
+  password: string;
+}
