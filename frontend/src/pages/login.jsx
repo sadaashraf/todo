5b12309b1160001b7b3a71b6121df34ebee64b9a
Muhammad Ashraf <nayeemashraf92@gmail.com>
2026-01-25 10:01:20 +0500

    token save

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -30,7 +30,7 @@ function Login() {
         return;
       }
 
-      // Save token
+      // token save
       if (data.token) localStorage.setItem("token", data.token);
 
       // Navigate to dashboard
